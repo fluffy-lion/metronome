e7f1ce0edda3533316cbf0668d2bf804b9655411
fluffy-lion <fluffylionprojects@gmail.com>
2023-05-06 22:38:07 +0100

    added metronome

---
+++ src/Metronome.js
@@ -0,0 +1,13 @@
+const Metronome = () => {
+    let bpm = 120
+    let playing = false
+
+    return (
+        <div>
+            <h1>metro</h1>
+            <div>{bpm}</div>
+        </div>
+    )
+}
+
+export default Metronome
