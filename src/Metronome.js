04a6e263eaca3df6e45b407e9b9cb70ef926ce2d
fluffy-lion <fluffylionprojects@gmail.com>
2023-05-08 16:50:19 +0100

    created audio object

--- src/Metronome.js
+++ src/Metronome.js
@@ -1,18 +1,32 @@
 import { useState } from 'react'
 import './metronome.css'
-
+import click1 from './sounds/click1.wav'
+import click2 from './sounds/click2.wav'
 const Metronome = () => {
     const [bpm, setBpm] = useState(120)
     const [playing, setPlaying] = useState(false)
     const [count, setCount] = useState(0)
     const [beatsPerMeasure, setBeatsPerMeasure] = useState(4)
 
+    let clickOne = new Audio(click1)
+    let clickTwo = new Audio(click2)
+    
+    const handleBpmChange = (e) => {
+        setBpm(e.target.value)
+    }
+
     return (
         <div className='metronome'>
             <h1>metro</h1>
             <div className='bpm-slider'>
                 <div>{bpm}</div>
-                <input type="range" min="60" max="240"></input>
+                <input 
+                    type="range"
+                    min="60"
+                    max="240"
+                    value={bpm}
+                    onChange={handleBpmChange}
+                />
             </div>
             <button>{playing ? 'stop' : 'start'}</button>
         </div>
